7e2a17e5925a2feade4abcc39b7e1b57e93eef8d
CipherPhantom <oyepitan10@gmail.com>
2024-01-03 10:29:12 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     }).catch(() => console.log('Signup system offline'));
