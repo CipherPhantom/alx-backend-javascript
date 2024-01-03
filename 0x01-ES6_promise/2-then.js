07ca98bfca68b9ba0346c1eef7d193e9a9625325
CipherPhantom <oyepitan10@gmail.com>
2024-01-03 02:13:52 +0100

    2-then.js && 3-all.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' }))
+  return promise.then(() => ({ status: 200, body: success }))
     .catch(() => Error()).then(() => console.log('Got a response from the API'));
 }
 
