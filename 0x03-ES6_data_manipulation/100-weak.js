6867b6071dcda3e846f3c0fb0e191e200fd94914
CipherPhantom <oyepitan10@gmail.com>
2024-01-08 21:35:23 +0100

    0x03-ES6_data_manipulation - Fix Error1

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,13 +2,11 @@ const MAX_NUMBER_OF_QUERIES = 5;
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.has(endpoint)) {
-    const count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count + 1);
-    if (count >= MAX_NUMBER_OF_QUERIES) {
-      throw new Error('Endpoint load is high');
-    }
-  } else {
-    weakMap.set(endpoint, 1);
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_NUMBER_OF_QUERIES) {
+    throw new Error('Endpoint load is high');
   }
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,15 @@
 export default function cleanSet(set, startString) {
   const strings = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
   const arraySet = Array.from(set);
   arraySet.forEach((value) => {
-    if (value.startsWith(startString) && startString !== '') {
-      strings.push(value.slice(startString.length));
+    if (typeof startString === 'string' && value.startsWith(startString)) {
+      const substring = value.slice(startString.length);
+      if (substring && substring !== value) {
+        strings.push(substring);
+      }
     }
   });
   return strings.join('-');
