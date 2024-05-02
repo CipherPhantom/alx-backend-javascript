36c81b0d0a1b8c627b4b3a7b7f7934f36a3afdc3
CipherPhantom <oyepitan10@gmail.com>
2024-05-02 17:52:38 +0100

    8-api

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable jest/lowercase-name */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const API_URL = 'http://localhost:7865';
+  it('GET /', () => {
+    request.get(API_URL, (error, response, body) => {
+      expect(response.statusCode).to.be.equals(200);
+      expect(body).to.be.equals('Welcome to the payment system');
+    });
+  });
+});
